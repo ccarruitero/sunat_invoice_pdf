649f8a80e9cda6055e59b22d508444c31e4948be
Cesar Carruitero <cesar@mozilla.pe>
2017-12-21 10:24:58 -0500

    add lines and totals table

--- lib/sunat_invoice_pdf/pdf_document.rb
+++ lib/sunat_invoice_pdf/pdf_document.rb
@@ -1,8 +1,22 @@
 require 'prawn'
+require 'prawn/table'
 require_relative 'locale'
 
 module SunatInvoicePdf
   class PdfDocument < Prawn::Document
+    OPERATIONS = {
+      '1001' => 'operaciones gravadas',
+      '1002' => 'operaciones inafectas',
+      '1003' => 'operaciones exoneradas',
+      '1004' => 'Operaciones gratuitas',
+      '1005' => 'Sub total de venta',
+      '2001' => 'Percepciones',
+      '2002' => 'Retenciones',
+      '2003' => 'Detracciones',
+      '2004' => 'Bonificaciones',
+      '2005' => 'Total descuentos'
+    }.freeze
+
     def initialize(doc)
       super(page_size: "A6")
       text doc.provider.name
@@ -13,13 +27,43 @@ module SunatInvoicePdf
 
       text doc.document_number
 
+      rows = []
       doc.lines.each do |line|
-        text "#{line.desc} #{line.price}"
+        row = []
+        row << line.description
+        row << line.quantity
+        row << amount_content(line.bi_value)
+        rows << row
       end
-      text "TOTAL #{doc.total}"
+
+      hash_to_row(doc.sale_totals, rows)
+      hash_to_row(doc.taxes_totals, rows)
+      total_row('TOTAL', doc.total, rows)
+
+      options = { cell_style: { borders: [] }}
+      table(rows, options)
     end
 
     private
+
+    def get_label(code)
+      OPERATIONS[code]&.upcase || code
+    end
+
+    def amount_content(amount)
+      { content: amount.to_s, align: :right }
+    end
+
+    def hash_to_row(hash, data)
+      hash.each do |key, value|
+        label = get_label(key.to_s.upcase)
+        total_row(label, value, data)
+      end
+    end
+
+    def total_row(label, total_amount, data)
+      data << [{ content: label, colspan: 2}, amount_content(total_amount)]
+    end
 
     def print_address(provider)
       text "#{provider.street} #{provider.zone}"
--- sunat_invoice_pdf.gemspec
+++ sunat_invoice_pdf.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "sunat_invoice", "~> 0.0.3"
   spec.add_dependency "prawn", "~> 2.2.2"
+  spec.add_dependency "prawn-table", "~> 0.2.2"
   spec.add_dependency "i18n", "~> 0.9"
 
   spec.add_development_dependency "bundler", "~> 1.14"
